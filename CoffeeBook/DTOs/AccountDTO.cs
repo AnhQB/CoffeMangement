94c0cd5df63722545a019b4d685a2154ad648928
anhqb <101201273+AnhQB@users.noreply.github.com>
2023-03-03 21:54:21 +0700

    update db and list account

--- CoffeeBook/DTOs/AccountDTO.cs
+++ CoffeeBook/DTOs/AccountDTO.cs
@@ -8,6 +8,7 @@ namespace CoffeeBook.DTOs
 {
     public class AccountDTO
     {
+        public int Id { get; set; }
         public string UserName { get; set; }
 
         public string DisplayName { get; set; }
